f0212097b2099a7272cfbdc601a44f8f51c4ba34
Jesse Brown <JBwerx@gmail.com>
2021-08-12 20:31:09 -0700

    first push

---
+++ index/index.js
@@ -0,0 +1,7 @@
+//alert("js loaded");
+
+// JUMPSCARE LISTENER //
+var jumpscare = function jumpscare() {
+  alert("HELLO WORLD!!!");
+};
+document.getElementById("collagePic_1").addEventListener('click', jumpscare);
